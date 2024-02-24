2410064bb4bf3ae0381ccc9a56c97da309055a66
simontessard <simon.tessard@gmail.com>
2024-02-24 14:23:44 +0100

    Sort by post button

--- front/src/app/features/posts/components/list/list.component.ts
+++ front/src/app/features/posts/components/list/list.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import {map, Observable} from 'rxjs';
 import { PostInformation } from '../../../../interfaces/postInformation.interface';
 import { PostService } from '../../../../services/post.service';
 import { Post } from '../../interfaces/post.interface';
@@ -13,6 +13,7 @@ import { PostApiService } from '../../services/post-api.service';
 export class ListComponent {
 
   public posts$: Observable<Post[]> = this.postApiService.all();
+  public sortAscending = true;
 
   constructor(
     private postService: PostService,
@@ -21,5 +22,17 @@ export class ListComponent {
 
   get user(): PostInformation | undefined {
     return this.postService.postInformation;
+  }
+
+  /* Sort method by date for posts */
+  sortPostsByDate() {
+    this.sortAscending = !this.sortAscending;
+    this.posts$ = this.posts$.pipe(
+      map(posts => posts.slice().sort((a, b) => {
+        const dateA = a.createdAt ? new Date(a.createdAt).getTime() : 0;
+        const dateB = b.createdAt ? new Date(b.createdAt).getTime() : 0;
+        return this.sortAscending ? dateA - dateB : dateB - dateA;
+      }))
+    );
   }
 }
