3c0b4f526624bd6d8742fb993f9cb99ddd7cf2af
simontessard <simon.tessard@gmail.com>
2024-02-25 20:38:27 +0100

    Get one topic route

--- back/src/main/java/com/openclassrooms/mddapi/controllers/TopicController.java
+++ back/src/main/java/com/openclassrooms/mddapi/controllers/TopicController.java
@@ -4,10 +4,12 @@ import com.openclassrooms.mddapi.models.Topic;
 import com.openclassrooms.mddapi.services.TopicService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/topics")
@@ -23,5 +25,11 @@ public class TopicController {
     public ResponseEntity<List<Topic>> getAllTopics() {
         List<Topic> topics = topicService.getAllTopics();
         return ResponseEntity.ok(topics);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Topic> getTopic(@PathVariable Long id) {
+        Optional<Topic> topicOptional = topicService.getTopicById(id);
+        return topicOptional.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
